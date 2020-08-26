8789da738093a21d219d8db283f1e4f67a6ea042
FLOWZ <ketuimevbore@yahoo.com>
2020-08-26 04:08:22 -0700

    currentWeatherViewModel connected to currentWeather activity display

--- app/src/main/java/com/flowz/forcastapp/display/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/flowz/forcastapp/display/weather/current/CurrentWeatherFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 
 import com.flowz.forcastapp.R
@@ -14,6 +16,7 @@ import com.flowz.forcastapp.data.network.ConnectivityInterceptorImpl
 import com.flowz.forcastapp.data.network.WeatherNetworkDataSoucreImpl
 import com.flowz.forcastapp.data.network.WeatherstackApiService
 import com.flowz.forcastapp.display.base.ScopedFragment
+import com.flowz.forcastapp.internal.glide.GlideApp
 import kotlinx.android.synthetic.main.current_weather_fragment.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -73,7 +76,62 @@ class CurrentWeatherFragment : ScopedFragment(), KodeinAware {
 
         val currentWeather = viewModel.weather.await()
 
-        textView.text = currentWeather.toString()
+        group_loading.visibility = View.GONE
+
+        updateLocation("Toronto")
+        updateDateToToday()
+        updateTemperature(currentWeather.temperature, currentWeather.feelslike)
+        updateCondition(currentWeather.weatherDescriptions[0])
+        updatePrecipitation(currentWeather.precip)
+        updateWind(currentWeather.windDir, currentWeather.windSpeed, currentWeather.windDegree)
+        updateVisibility(currentWeather.visibility)
+
+        GlideApp.with(this@CurrentWeatherFragment)
+            .load("${currentWeather.weatherIcons[0]}")
+            .into(weather_conditionIcon)
+
     }
+
+    private fun updateLocation(location: String){
+
+        (activity as? AppCompatActivity)?.supportActionBar?.title = location
+    }
+
+    private fun updateDateToToday(){
+
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = "Today"
+    }
+
+    private fun updateTemperature(temperature: Int, feelsLike: Int){
+
+        textView_temperature.text = "$temperature °C"
+        textView_feels_like_temperature.text = "Feels like $feelsLike °C"
+    }
+
+    private fun updateCondition(condition: String){
+
+        textView_condition.text = condition
+    }
+
+    private fun updatePrecipitation(precipitationVolume: Double){
+
+        text_precipitation.text = "Precipitation: $precipitationVolume"
+
+//        Toast.makeText(this.context, "Precipitation: $precipitationVolume", Toast.LENGTH_LONG).show()
+        Log.e("precipe", "Precipitation: $precipitationVolume")
+    }
+
+    private fun updateWind(windDirection: String, windSpeed: Int, windDegree: Int){
+
+        textView_wind.text = "Wind: $windDirection, $windSpeed, $windDegree"
+    }
+
+    private fun updateVisibility(visibilityDistance: Int){
+
+        textView_visibility.text = "Visibility: $visibilityDistance"
+    }
+
+
+
 
 }
